25c6891204f5e7d5f22f247c5c0dc80715d59fb3
Arwa <arwa.it15@gmail.com>
2017-10-05 11:40:47 -0500

    express-static

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// app.get('/', (req, res, next) => {
+//   res.render('');
+// });
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
